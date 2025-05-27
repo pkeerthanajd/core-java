c2a91ca3986eb7b0228e03129a5a30d157e0c1de
keerthana <pkeerthanajd@gmail.com>
2025-05-27 13:24:16 +0530

    declared a comment stmt

--- wloophi.java
+++ wloophi.java
@@ -2,11 +2,12 @@ public class wloophi
 {
     public static void main(String[] args) {
         int i = 1;
+        int j = 1;
         while(i<=4)
         {
             System.out.println("hi "+i);
             i++;
-            int j = 1;
+            // int j = 1; i can't access this line so made the changes 
             while(j<=3)
             {
                 System.out.println("hello "+j);
