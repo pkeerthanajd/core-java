ad58bbcf560f44571e0284bdafd87cb6b4c37132
keerthana <pkeerthanajd@gmail.com>
2025-06-03 11:03:53 +0530

    Multiple catch block

---
+++ Excep.java
@@ -0,0 +1,47 @@
+// public class Excep 
+// {
+//     public static void main(String[] args) {
+//         int i = 0;
+//         int j = 0;
+
+//         try  //Exception handling
+//         {
+//             j = 18/i;
+//         }
+//         catch(Exception e)//Here Exception is a class and e is the object 
+//         {
+//             System.out.println("Something went wrong");
+//         }
+//         System.out.println(j);
+//         System.out.println("Byee");
+//     }
+// }
+// -----------------------------------------------------------------------------------------------------------------------------------------------
+public class Excep 
+{
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 0;
+        int nums[] = new int[5];
+        try  //Exception handling
+        {
+            j = 18/i;
+            System.out.println(nums[1]);
+            System.out.println(nums[5]);
+        }
+        catch(ArithmeticException e) //extends Runtime which again extends a Exception
+        {
+            System.out.println("Can't divide by zero"); //NOTE only one exception is handled at a time per try block.
+        }
+        catch(ArrayIndexOutOfBoundsException e) //extends Runtime which again extends a Exception
+        {
+            System.out.println("Exceed the array bound limit");
+        }
+        catch(Exception e)//Exception is a PARENT CLASS so it should be at the bottom and also Exception extends throwable
+        {
+            System.out.println("Something went wrong");
+        }
+        System.out.println(j);
+        System.out.println("Byee");
+    }
+}
